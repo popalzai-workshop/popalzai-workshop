7e7c6a0418be182f5157159793c711472f1dcc3b
popalzai-workshop <faisal.md.rafi@gmail.com>
2026-07-14 18:12:35 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+// Popalzai Workshop Website
+
+console.log("🏍️ Popalzai Workshop Website Started");
+
+
+// د زنګ تڼۍ پیغام
+const button = document.querySelector(".btn");
+
+if(button){
+    button.addEventListener("click", function(){
+        console.log("د زنګ تڼۍ وکارول شوه");
+    });
+}
